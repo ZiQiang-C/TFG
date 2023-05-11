71fae812d55802cdd72fe85c7884561317e6d50e
TheEleventhJF <chema2000jimenez@gmail.com>
2023-05-11 02:39:13 +0200

    mas pantallas 1

---
+++ Proyecto/app/src/main/java/com/example/degabriel/noticiaLista.java
@@ -0,0 +1,14 @@
+package com.example.degabriel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class noticiaLista extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_noticia_lista);
+    }
+}
