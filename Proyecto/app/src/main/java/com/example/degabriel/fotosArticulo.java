a335040b5346485a2dc426ab8496d161da58cefe
TheEleventhJF <chema2000jimenez@gmail.com>
2023-05-22 23:11:35 +0200

    interfaces hechas

---
+++ Proyecto/app/src/main/java/com/example/degabriel/fotosArticulo.java
@@ -0,0 +1,14 @@
+package com.example.degabriel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class fotosArticulo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fotos_articulo);
+    }
+}
